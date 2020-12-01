1873d6089a10a3588325df9970c49f8cbdd45a0b
Dmitriy_Konopelko <Dmitriy_Konopelko@outlook.com>
2020-12-01 21:57:53 +0300

    Начинаем создание ViewModel

---
+++ Bookinist/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathCore.ViewModels;
+
+namespace Bookinist.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        
+        #region A: TYPE - Description
+
+        ///<summary></summary>
+        private double _a;
+
+        ///<summary></summary>
+        public double A {get => _a; set => Set(ref _a, value);}
+
+
+        #endregion
+    }
+}
